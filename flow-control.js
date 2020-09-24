faa015e15b2251698e40debdb65c1b5f3fb019e0
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-09-24 05:05:07 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,12 +24,12 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  (age > 13 && age < 19 ) ? "You are a teenager" : "You are not a teenager"
+  (age >= 13 && age <= 19 ) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
   switch(age){
-    case (age > 13 && age < 19):
+    case (age >= 13 && age <= 19):
       return ("You are a teenager")
     case (age < 13 || age > 19):
       return ("You have an age")
