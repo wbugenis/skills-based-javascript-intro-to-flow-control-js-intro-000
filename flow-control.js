14800081a1421aa0ca1da4966b1dcb5d5343a971
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-09-24 04:45:38 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,17 +1,30 @@
 function basicTeenager(age) {
-
+  if(age > 13 && age < 19){
+    return('You are a teenager!')
+  }
 }
 
 function teenager(age) {
-
+  if(age > 13 && age < 19){
+    return('You are a teenager!')
+  } else {
+    return("You are not a teenager")
+  }
 }
 
 function ageChecker(age) {
-
+  if(age > 13 && age < 19){
+      return ("You are a teenager!")
+  } else if (age > 19) {
+      return ("You are a grownup")
+  } else if (age < 13) {
+      return ("You are a kid")
+  } else return
+  }
 }
 
 function ternaryTeenager(age) {
-
+  (age > 13 && age < 19 ) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
