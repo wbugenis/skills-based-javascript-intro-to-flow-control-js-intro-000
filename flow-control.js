b227698d10fbef3265c3075a858190fc43c11dea
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-09-24 05:14:20 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,7 +29,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age){
-    case 13,14,15,16,17,18,19:
+    case 15:
       return ("You are a teenager")
       break;
     default:
