e1ab436bea3bb88bcb8d5cab9f8696f41f5aab17
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-09-24 05:08:38 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,7 +31,9 @@ function switchAge(age) {
   switch(age){
     case (age >= 13 && age <= 19):
       return ("You are a teenager")
+      break;
     case (age < 13 || age > 19):
       return ("You have an age")
+      break;
   }
 }
